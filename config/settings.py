b4489c4f8151a1ff976201f3a9ab6b35f63abc77
javiste <122609141+ndire92@users.noreply.github.com>
2026-03-12 20:17:10 +0000

    Update email configuration to use env variables
    Refactor email settings to use environment variables.

--- config/settings.py
+++ config/settings.py
@@ -186,14 +186,12 @@ DEXPAY_TEST_BASE_URL = "https://api-sandbox.dexpay.africa"
 DEXPAY_LIVE_BASE_URL = "https://api.dexpay.africa"
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT"))
+EMAIL_USE_SSL = True
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-
-EMAIL_HOST_USER = os.getenv("EMAIL_USER")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-ADMIN_EMAIL = "cinddera1@gmail.com"
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
 
